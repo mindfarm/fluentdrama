64040f325b6f7f8fe268816925b21c0019049d2c
Shane <dev@mindfarm.nz>
2021-10-29 13:22:03 +1100

    feat (bot): create logs table in postgres

---
+++ bot/repository/postgres/migrations/20211029122510_logs.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+CREATE TABLE [IF NOT EXISTS] logs {
+    channel TEXT NOT NULL,
+    nick TEXT NOT NULL,
+    stamp TIMESTAMP DEFAULT NOW(),
+    said TEXT,
+}
+
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back.
+DROP TABLE [IF EXISTS] logs
